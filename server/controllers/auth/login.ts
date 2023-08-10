5b1bd791160ddb5ed0688331b8f4d59703135cf3
Masayoshi <masasensei26@mail.com>
2023-08-10 15:55:03 +0700

    del try catch login

--- server/controllers/auth/login.ts
+++ server/controllers/auth/login.ts
@@ -6,49 +6,45 @@ import { signToken } from "../../utils/sign_token";
 
 const loginUser = () => {
   return async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      // REQUEST BODY
-      const { email, password } = req.body;
+    // REQUEST BODY
+    const { email, password } = req.body;
 
-      // Check user with email
-      const user = await UserProvider.findOne({
-        where: {
-          email: email,
-        },
-      });
-      // when user not found
-      if (!user) {
-        return next(new AppError("User not found", 404));
-      }
-
-      // Periksa pengguna apakah aktif atau tidak
-      if (!user.is_active) {
-        return next(new AppError("User is not active", 403));
-      }
-
-      // Check password
-      const isPasswordCorrect = bcrypt.compareSync(password, user.password);
-
-      if (!isPasswordCorrect) {
-        return next(new AppError("Wrong password", 401));
-      }
-
-      // Generate token
-      const token: string = await signToken(user);
-
-      // Set Header Session
-      req.session = {
-        jwt: token,
-      };
-
-      return res.status(200).json({
-        message: "success",
-        token: token,
-        data: user,
-      });
-    } catch (error) {
-      return next(new AppError("Something went wrong", 500));
+    // Check user with email
+    const user = await UserProvider.findOne({
+      where: {
+        email: email,
+      },
+    });
+    // when user not found
+    if (!user) {
+      return next(new AppError("User not found", 404));
     }
+
+    // Periksa pengguna apakah aktif atau tidak
+    if (!user.is_active) {
+      return next(new AppError("User is not active", 403));
+    }
+
+    // Check password
+    const isPasswordCorrect = bcrypt.compareSync(password, user.password);
+
+    if (!isPasswordCorrect) {
+      return next(new AppError("Wrong password", 401));
+    }
+
+    // Generate token
+    const token: string = await signToken(user);
+
+    // Set Header Session
+    req.session = {
+      jwt: token,
+    };
+
+    return res.status(200).json({
+      message: "success",
+      token: token,
+      data: user,
+    });
   };
 };
 
